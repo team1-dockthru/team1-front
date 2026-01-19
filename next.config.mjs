ccf36eef99b48dfe481f13afdd9697f0e9a10d9e
Reim <riozio81@gmail.com>
2026-01-19 15:37:40 +0900

    랜딩페이지

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
